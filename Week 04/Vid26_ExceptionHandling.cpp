79736c9cc75fe8d614fe4633cdac668a24799576
TheSukhSingh <130133318+TheSukhSingh@users.noreply.github.com>
2023-07-17 11:05:47 +0530

    Update

--- Week 04/Vid26_ExceptionHandling.cpp
+++ Week 04/Vid26_ExceptionHandling.cpp
@@ -2,5 +2,32 @@
 using namespace std;
 
 int main(){
-    
+    int a = 10, b = 0;
+
+    try{
+        try{
+            if(b==0)    throw 0;
+            cout << a/b << endl;
+        }
+        // catch(int e){
+        //     cout << e << " thrown from the try block!" << endl;
+        // }
+        catch(...){
+            cout << " thrown from the try block!" << endl;
+            throw;
+        }
+    }
+    catch(...){
+        cout << "Caught!" << endl;
+    }
+
+    // catch(char err){
+    //     cout << err << " thrown from the try block!" << endl;
+    // }
+
+
+
+    cout << "Inside int main, outside try catch block" << endl;
+
+    return 0;
 }
